5f14c60180301da1b7f05da5ef8f852916e7acc1
Philip Van Raalte <phil.vanraalte@gmail.com>
2019-05-13 21:01:09 -0400

    added days component

---
+++ src/Days.tsx
@@ -0,0 +1,60 @@
+import * as React from "react";
+import moment from "moment";
+
+interface Props {
+  viewDate: moment.Moment;
+}
+
+export const Days: React.SFC<Props> = props => {
+  const t = performance.now();
+
+  const year = props.viewDate.year();
+  const month = props.viewDate.month();
+
+  const nextMonth = props.viewDate.clone().add(1, "month");
+  let prevMonth = props.viewDate.clone().subtract(1, "month");
+  const days = prevMonth.daysInMonth();
+
+  // change to first day of last week in previous month
+  prevMonth.date(days).startOf("week");
+  const daysToAdd = days + 1 - prevMonth.date();
+
+  const firstDay = prevMonth.date();
+  let dayCells: { children: number; className: string }[] = Array.from(
+    {
+      length: daysToAdd
+    },
+    (_v, i) => ({ children: i + firstDay, className: "old" })
+  ).concat(
+    Array.from(
+      {
+        length: props.viewDate.daysInMonth()
+      },
+      (_v, i) => ({ children: i + 1, className: "" })
+    )
+  );
+
+  dayCells = dayCells.concat(
+    Array.from(
+      {
+        length: 7 - (dayCells.length % 7)
+      },
+      (_v, i) => ({ children: i + 1, className: "new" })
+    )
+  );
+
+  console.log("time", performance.now() - t);
+
+  return (
+    <div className="days">
+      {dayCells.map((cellProps, index) => (
+        <div
+          key={`${props.viewDate.format("YYYY-MM")}-${index}`}
+          {...cellProps}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default Days;
--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,23 @@
 import * as React from "react";
+import moment from "moment";
 import "./calendar.less";
+import Days from "./Days";
 
 interface Props {
   text?: string;
 }
 
+window.moment = moment;
+
 export const DateSelector: React.SFC<Props> = props => {
+  const date = moment();
+
   return (
     <div className="calendar">
-      <div className="title">Date Selector</div>
+      <time className="title" dateTime={date.format("YYYY-MM")}>
+        {date.format("MMMM YYYY")}
+      </time>
+      <Days viewDate={date} />
     </div>
   );
 };
