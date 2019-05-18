775154214dfa0ec0cc3d478a7b2e9be22b16ea23
Philip Van Raalte <phil.vanraalte@gmail.com>
2019-05-18 10:46:01 -0400

    added custom storybook theme

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,7 +1,18 @@
-import { configure, addDecorator } from "@storybook/react";
+import { configure, addDecorator, addParameters } from "@storybook/react";
 import { withA11y } from "@storybook/addon-a11y";
+import { create } from "@storybook/theming";
 
 addDecorator(withA11y);
+
+addParameters({
+  options: {
+    theme: create({
+      base: "light",
+      brandTitle: "date-picker",
+      brandUrl: "https://github.com/FIL1994/date-picker"
+    })
+  }
+});
 
 // automatically import all files ending in *.stories.js
 const req = require.context("../stories", true, /\.stories\.(js|tsx)$/);
